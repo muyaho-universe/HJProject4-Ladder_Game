3a6ce60a32f60d51cfccca84ddd0c5bbe4663e0c
muyaho-universe <kimdaesk@gmail.com>
2022-06-30 20:51:55 +0900

    Do the initial commit

---
+++ app/src/main/java/com/dale/ladder/MainGameFrame.java
@@ -0,0 +1,12 @@
+package com.dale.ladder;
+
+import javax.swing.JFrame;
+
+public class MainGameFrame extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
