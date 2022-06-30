30e9b671e45e9538e771f217d670ee57119d83bc
muyaho-universe <kimdaesk@gmail.com>
2022-06-30 21:59:43 +0900

    Working on first input panel

---
+++ app/src/main/java/com/dale/ladder/FirstInputPanel.java
@@ -0,0 +1,20 @@
+package com.dale.ladder;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class FirstInputPanel extends JPanel {
+	
+	FirstInputPanel(){
+		setBackground(Color.WHITE);
+		setBounds(0, 0, 1080, 504);
+		setLayout(new BorderLayout());
+		createPanel();
+	}
+	
+	private void createPanel() {
+		JPanel centerPanel = new JPanel();
+	}
+}
--- app/src/main/java/com/dale/ladder/MainGameFrame.java
+++ app/src/main/java/com/dale/ladder/MainGameFrame.java
@@ -1,12 +1,31 @@
 package com.dale.ladder;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
 
 public class MainGameFrame extends JFrame {
-
+	private static final String TITLE = "사다리 타기";
+	
+	private FirstInputPanel firstInputPanel;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MainGameFrame mainFrame = new MainGameFrame();
+		mainFrame.run();
 	}
-
+	
+	MainGameFrame(){
+		setSize(1080, 504);
+		setTitle(TITLE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setLayout(null);
+		setResizable(false);
+	}
+	private void run() {
+		this.setVisible(true);
+		firstInputPanel = new FirstInputPanel();
+		
+		this.add(firstInputPanel);
+	}
 }
